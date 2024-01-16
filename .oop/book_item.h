e92e1fec3219ef4caa652b2567179fa6cde8a486
maimunaali <alimaimunamuhammad@gmail.com>
2024-01-17 00:14:30 +0100

    maimuna ali
    assignment 2 (project)

---
+++ .oop/book_item.h
@@ -0,0 +1,27 @@
+#ifndef BOOK_ITEM_H
+#define BOOK_ITEM_H
+#include "library_item.h"
+class BookItem : public LibraryItem {
+private:
+    std::string author;
+    std::string isbn;
+
+public:
+    BookItem(const std::string& title, const std::string& author, const std::string& isbn)
+        : LibraryItem(title), author(author), isbn(isbn) {}
+
+    const std::string& getAuthor() const {
+        return author;
+    }
+
+    const std::string& getIsbn() const {
+        return isbn;
+    }
+
+    void displayBookInfo() const {
+        std::cout << "Title: " << getTitle() << "\nAuthor: " << author << "\nISBN: " << isbn << std::endl;
+    }
+};
+#endif
+
+
